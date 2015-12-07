f6dbc96210d09ecc1307de9e611ff23c2785451a
kittiwake <kittiwake@mail.ru>
2015-12-07 14:12:53 +0300

    1

---
+++ config.php
@@ -0,0 +1,32 @@
+<?php
+
+// Задаем константы:
+define ('DS', DIRECTORY_SEPARATOR); // разделитель для путей к файлам
+$sitePath = realpath(dirname(__FILE__) . DS) . DS;
+define ('SITE_PATH', $sitePath); // путь к корневой папке сайта
+//echo SITE_PATH;
+//C:\OpenServer\domains\localhost\gm17\
+$siteDir =  basename(dirname(__FILE__));
+define ('SITE_DIR', $siteDir);
+
+//константы для подключения к базе данных
+define('HOST', 'localhost'); 		//сервер
+define('USER', 'root'); 			//пользователь
+define('PASSWORD', 'mysql'); 			//пароль
+define('NAME_BD', 'bdgm');		//база
+
+
+function __autoload($class_name){
+
+    $array_paths = array('models', 'classes');
+
+    foreach ($array_paths as $path){
+
+        $path = SITE_PATH . $path .DS. $class_name . '.php';
+        if (is_file($path)){
+            include_once $path;
+        }
+    }
+}
+
+?>
---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+// включим отображение всех ошибок
+ini_set('display_errors',1);
+error_reporting(E_ALL);
+
+// подключаем конфиг
+include ('config.php');
+
+// Соединяемся с БД
+$dbObject = new PDO('mysql:host=' . HOST . ';dbname=' . NAME_BD, USER, PASSWORD);
+$dbObject->exec('SET CHARACTER SET utf8');
+
+
+// Загружаем router
+$router = new Router();
+
+// запускаем маршрутизатор
+$router->run();
